87e8961ddca9df341ca401b7ffa17b36a47e565a
handharbeni <mhandharbeni@gmail.com>
2018-11-09 00:41:56 +0700

    Load HTML

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var compression = require('compression');
 var helmet = require('helmet');
-
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 6789,
@@ -10,7 +9,12 @@ app.use(compression());
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set('view engine', 'ejs')
+// app.set('view engine', 'html')
+
+app.use(express.static(__dirname));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 var routes = require('./module/routes/Routes');
 routes(app);
--- module/controllers/Controllers.js
+++ module/controllers/Controllers.js
@@ -1,6 +1,16 @@
 'use strict';
+var fs = require('fs');
+var temp = require('templatesjs');
 
 exports.test = function(req, res){
     // res.status(200).send({'results':'empty'});
-    res.render('samples');
+    //res.render('test/samples.html');
+    res.render('template/index');
+    // fs.readFile("./views/test.html", (err, data) => {
+    //     if(err) return res.status(404).send(err);
+    //     var output = temp.setSync("A");
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     // res.send(data);
+    //     res.end(data);
+    // }); 
 }
